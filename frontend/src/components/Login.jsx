63e0b23548eb6f2a5c769a9b31d3818635fd7284
Ananth-kumar-ai <ananthkumarnalluri456@gmail.com>
2025-05-05 15:40:13 +0530

    Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = ({ setAuth }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://studentportal-g670.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ Regno, password }),
